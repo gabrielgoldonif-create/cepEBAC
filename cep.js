f255d685c1e3e450b4d2fd542f84e16262818d75
Gabriel <gabrielgoldonif@gmail.com>
2025-10-04 14:46:01 -0400

    Correcao-CEP

--- cep.js
+++ cep.js
@@ -5,13 +5,16 @@ const cidade = document.getElementById("cidade");
 const uf = document.getElementById("uf");
 const statusEl = document.getElementById("status");
 
-// Ao DIGITAR no campo CEP //
+const STORAGE_KEY = "formEndereco"; // chave de armazenamento
+
+// Ao DIGITAR no campo CEP
 cep.addEventListener("input", () => {
   const cepNum = cep.value.replace(/\D/g, ""); // só números
 
-  // só consulta quando completar 8 dígitos /?
+  // só consulta quando completar 8 dígitos
   if (cepNum.length !== 8) {
-    statusEl.textContent = ""; // limpa status enquanto digita
+    statusEl.textContent = "";
+    salvarDados(); // salva o que já tem
     return;
   }
 
@@ -32,8 +35,40 @@ cep.addEventListener("input", () => {
       uf.value = dados.uf || "";
 
       statusEl.textContent = "Endereço encontrado!";
+      salvarDados(); // salva tudo preenchido
     })
     .catch(() => {
       statusEl.textContent = "Erro ao buscar CEP.";
     });
+});
+
+// Salvar no localStorage sempre que o usuário digitar em qualquer campo
+[cep, logradouro, bairro, cidade, uf].forEach(campo => {
+  campo.addEventListener("input", salvarDados);
+});
+
+
+// salva os dados no Web Storage//
+function salvarDados() {
+  const dados = {
+    cep: cep.value,
+    logradouro: logradouro.value,
+    bairro: bairro.value,
+    cidade: cidade.value,
+    uf: uf.value
+  };
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(dados));
+}
+
+// restaura os dados //
+window.addEventListener("load", () => {
+  const dadosSalvos = localStorage.getItem(STORAGE_KEY);
+  if (dadosSalvos) {
+    const dados = JSON.parse(dadosSalvos);
+    cep.value = dados.cep || "";
+    logradouro.value = dados.logradouro || "";
+    bairro.value = dados.bairro || "";
+    cidade.value = dados.cidade || "";
+    uf.value = dados.uf || "";
+  }
 });
